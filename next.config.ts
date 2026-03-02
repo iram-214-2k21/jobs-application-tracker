8bc14ea9d550dd70652fa5c903bf3ea25ccfb146
iram-214-2k21 <107210856+iram-214-2k21@users.noreply.github.com>
2026-03-02 07:57:36 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
